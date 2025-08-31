e5c7fc14116992d841512c242ebc0f0cd9d5d3be
devv-ho <khj199859@naver.com>
2025-08-31 17:22:59 +0900

    feat(math) : Add Function For Distance Calculation Between Two Points 1. Add distance_to() public method to Point 2. Write tests distance calculation between same points and different points 3. Add dev-dependencies Approx for floating point test

--- src/math.rs
+++ src/math.rs
@@ -20,11 +20,17 @@ impl Point {
     pub const fn zero() -> Self {
         Self::new(0.0, 0.0)
     }
+
+    /// Calculate Euclidean distance to given point
+    pub fn distance_to(&self, other: &Self) -> f32 {
+        ((self.x - other.x).powi(2) + (self.y - other.y).powi(2)).sqrt()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use approx::assert_relative_eq;
 
     #[test]
     fn test_create_new_point() {
@@ -68,5 +74,21 @@ mod tests {
 
         assert_eq!(point_1.x, point_2.x);
         assert_eq!(point_1.y, point_2.y);
+    }
+
+    #[test]
+    fn test_calculate_distance_between_same_point() {
+        let point_1 = Point::new(1.23, 23.1);
+        let point_2 = point_1.clone();
+
+        assert_relative_eq!(point_1.distance_to(&point_2), 0.0, epsilon = 1e-6);
+    }
+
+    #[test]
+    fn test_calculate_distance_between_different_point() {
+        let point_1 = Point::new(-2.0, -1.5);
+        let point_2 = Point::new(1.0, 2.5);
+
+        assert_relative_eq!(point_1.distance_to(&point_2), 5.0, epsilon = 1e-6);
     }
 }
